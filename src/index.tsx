06ff1ad7f9048c7f87aacc564154fc9bfa92cda4
Huy Nguyen <huy.nguyen@vfsc.vn>
2018-08-24 16:45:30 +0700

    config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './modules/App';
+import Index from './modules/App';
+import { Provider } from 'react-redux';
+import initStore from './config/store';
+
+const store = initStore()
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <Index />
+    </Provider>,
   document.getElementById('root') as HTMLElement
 );
