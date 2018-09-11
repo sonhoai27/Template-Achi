00712a81179fdcefd2d8de387b91cb6591c96ddc
sonhoai27 <sonhoai272@gmail.com>
2018-09-11 13:23:59 +0700

    home banner

--- template/src/modules/client/home/ClientBanner.tsx
+++ template/src/modules/client/home/ClientBanner.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 class ClientBanner extends React.Component {
   render() {
     return (
-      <>
+      <div className="banner">
         <div className="home-banner-video">
           <h1>HEADLINE T300X</h1>
           <video
@@ -19,7 +19,32 @@ class ClientBanner extends React.Component {
             />
           </video>
         </div>
-      </>
+        <div className="footer-banner col-xs-12">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-3 footer-banner-img">
+                <img
+                  src="https://brendon.com/wp-content/uploads/2018/02/Brendon-Burchard-Four-Breakthroughs_tablet.png"
+                  alt=""
+                  className="img-responsive"
+                />
+              </div>
+              <div className="col-sm-9">
+                <h2>Free Access to Brendon’s “Four Breakthroughs”:</h2>
+                <h4>
+                  Join 2 million subscribers! Get 4 new strategies every week on
+                  how to be more productive, confident, and happy.
+                </h4>
+                <div className="form-get-access">
+                  <input type="text" placeholder="Họ và tên" />
+                  <input type="email" placeholder="email" />
+                  <div className="btn btn-sm btn-info">Đăng ký <i className="ti-angle-right"/></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ template/src/modules/client/home/ClientIntroProfile.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+class ClientIntroProfile extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default ClientIntroProfile
